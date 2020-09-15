c4233f6d13d496d5a46025770e7290ccd26b5ef1
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-09-15 15:57:24 -0700

    Added array of times.

--- script.js
+++ script.js
@@ -37,6 +37,9 @@ newRow.append(saveBtnCell);
 
 container.append(newRow);
 
+// array of times
+let arr = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
 // onclick event to save user input to local storage
 $('.rowBttn').on('click', function () {
     let timeOfDay = $(this).parent().attr('id');
