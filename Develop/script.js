6d81e3f09807f4cf5f8ae5efd05bf4de3832f169
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-09-14 19:47:10 -0700

    Added onclick event to save user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,20 @@
-// GIVEN I am using a daily planner to create a schedule
-
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
 let currentDay;
 const today = Date.parse(new Date());
 currentDay = today;
 console.log(today);
 
-/*
-<section class="row">
-    <div class="col-md-4">
-    8:00am
-    </div>
-    <div class="col-md-5">
-        <input type="text" class="input" id="8am">
-        </div>
-    <div class="col-md-3">
-        <button class="save-btn" date-time="8am" id="save">Save</button>
-        </div>
-</section>
-*/
+$(document).ready(function () {
+
+    $('.save-btn').on("click", function () {
+
+        alert('saved');
+
+    });
+})
 
 let container = $('.container')
 let time;
+//Make repeatable
 time = '8:00am'
 let newRow = $('<section>').addClass('row');
 let hourCell = $('<div>').addClass('col-md-4');
@@ -60,3 +36,27 @@ saveBtnCell.append(saveBtn);
 newRow.append(saveBtnCell);
 
 container.append(newRow);
+
+// onclick event to save user input to local storage
+$('.rowBttn').on('click', function () {
+    let timeOfDay = $(this).parent().attr('id');
+    let textContent = $('input').val().trim();
+
+    localStorage.setItem(timeOfDay, textContent);
+    console.log(timeOfDay, textContent);
+});
+
+// take children and set to parent
+$('#8am').children('input').val(localStorage.getItem('8am'));
+$('#9am').children('input').val(localStorage.getItem('9am'));
+$('#10am').children('input').val(localStorage.getItem('10am'));
+$('#11am').children('input').val(localStorage.getItem('11am'));
+$('#12pm').children('input').val(localStorage.getItem('12pm'));
+$('#1pm').children('input').val(localStorage.getItem('1pm'));
+$('#2pm').children('input').val(localStorage.getItem('2pm'));
+$('#3pm').children('input').val(localStorage.getItem('3pm'));
+$('#4pm').children('input').val(localStorage.getItem('4pm'));
+$('#5pm').children('input').val(localStorage.getItem('5pm'));
+$('#6pm').children('input').val(localStorage.getItem('6pm'));
+$('#7pm').children('input').val(localStorage.getItem('7pm'));
+$('#8pm').children('input').val(localStorage.getItem('8pm'));
